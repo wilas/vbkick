a97c44fc399fc5206f35ba7d01d5d21d7e46e758
wilas <limak.kamwil@gmail.com>
2013-06-17 23:40:51 +0200

    use /dev/sr1 to install VBoxGuestAdditions; update sudoers checking; cleaning injection example

--- examples/SL6_injection_method/postinstall/repos.sh
+++ examples/SL6_injection_method/postinstall/repos.sh
@@ -1,9 +1,7 @@
 # List 3rd party repositories: http://wiki.centos.org/AdditionalResources/Repositories
 
 # Add EL repository
-## rpm --import http://elrepo.org/RPM-GPG-KEY-elrepo.org
 wget -O /etc/pki/rpm-gpg/RPM-GPG-KEY-elrepo.org http://elrepo.org/RPM-GPG-KEY-elrepo.org
-## rpm -Uvh http://elrepo.org/elrepo-release-6-5.el6.elrepo.noarch.rpm
 cat > /etc/yum.repos.d/elrepo.repo << "EOF"
 [elrepo]
 name=ELRepo.org Community Enterprise Linux Repository - el6
@@ -16,10 +14,7 @@ gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-elrepo.org
 EOF
 
 # Add EPEL repository
-## rpm --import http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6
 wget -O /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6 http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6
-## rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm or
-## rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 cat > /etc/yum.repos.d/epel.repo << "EOF"
 [epel]
 name=Extra Packages for Enterprise Linux 6 - $basearch
@@ -30,9 +25,7 @@ gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6
 EOF
 
 # Add Atrpms repository
-## rpm --import http://packages.atrpms.net/RPM-GPG-KEY.atrpms
 wget -O /etc/pki/rpm-gpg/RPM-GPG-KEY-atrpms http://packages.atrpms.net/RPM-GPG-KEY.atrpms
-## rpm -Uvh http://dl.atrpms.net/all/atrpms-repo-6-6.el6.$(uname -m).rpm
 cat > /etc/yum.repos.d/atrpms.repo << "EOF"
 [atrpms]
 name=Red Hat Enterprise Linux $releasever - $basearch - ATrpms
@@ -44,9 +37,7 @@ failovermethod=priority
 EOF
 
 # Add Remi repository
-## rpm --import http://rpms.famillecollet.com/RPM-GPG-KEY-remi
 wget -O /etc/pki/rpm-gpg/RPM-GPG-KEY-remi http://rpms.famillecollet.com/RPM-GPG-KEY-remi
-## rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 cat > /etc/yum.repos.d/remi.repo << "EOF"
 [remi]
 name=Les RPM de remi pour Enterprise Linux $releasever - $basearch
--- examples/SL6_injection_method/postinstall/sudo.sh
+++ examples/SL6_injection_method/postinstall/sudo.sh
@@ -1,3 +1,3 @@
 cp /etc/sudoers /etc/sudoers.orig
 # Allow sudo commands without a tty
-sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+sed -i -re "s/(^Defaults[ ]+requiretty)/#\1/" /etc/sudoers
