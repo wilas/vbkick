3f1b8f01581b01f7fb9476da4fb127e584e3e861
wilas <limak.kamwil@gmail.com>
2013-06-22 23:53:09 +0200

    rm VBOX_VERSION variable from postinstall

--- examples/SL6_lazy_method/postinstall/adm_postinstall.sh
+++ examples/SL6_lazy_method/postinstall/adm_postinstall.sh
@@ -1,11 +1,6 @@
 # set environment variables if available
 [[ -s "adm_envrc" ]] && . "./adm_envrc"
 
-LOG_PATH=.
-#LOG_PATH=/var/log
-echo "vm_type: ${VM_TYPE}" >> "${LOG_PATH}/adm_postinstall.log"
-echo "vm_description: ${VM_DESC}" >> "${LOG_PATH}/adm_postinstall.log"
-
 context_file="adm_context.txt"
 while read -r script; do
     # don't process comments
