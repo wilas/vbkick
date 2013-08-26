30b0e7edc7f73674691ee8a6aa1486f89bc0ac26
wilas <limak.kamwil@gmail.com>
2013-08-26 03:06:20 +0200

    install puppetlab and other repos from rpms - lazy example

--- examples/SL6_lazy_method/postinstall/tuning.sh
+++ examples/SL6_lazy_method/postinstall/tuning.sh
@@ -1,5 +1,4 @@
 # Run ansible, puppet, chef, python script, ruby script, etc.
-
 #puppet apply --hiera_config puppet_tuning/hiera.yaml --modulepath 'puppet_tuning/puppet_modules' puppet_tuning/site_puppet.pp;
 
 # set python path if needed (e.g. /usr/local/bin/python3)
