ffc5eceb048666138e34000421cd1168380e252d
wilas <limak.kamwil@gmail.com>
2013-10-07 20:14:14 +0200

    remove tuning postinstall scripts from examples

--- examples/SL6_injection_method/postinstall/hello_python.py
+++
@@ -1,3 +0,0 @@
-# python 2.6+
-with open ('/var/tmp/hello_postinstall_py.txt', 'a') as f:
-    f.write('Hello from postinstall python script!\n')
--- examples/SL6_injection_method/postinstall/tuning.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eEu
-
-# Run ansible, puppet, chef, python script, ruby script, etc.
-#puppet apply --hiera_config puppet_tuning/hiera.yaml --modulepath 'puppet_tuning/puppet_modules' puppet_tuning/site_puppet.pp;
-
-# set python path if needed (e.g. /usr/local/bin/python3)
-python hello_python.py
