34c7f2186ab4bd16126327f5bed458cbab468274
wilas <limak.kamwil@gmail.com>
2013-11-03 22:19:09 +0100

    virtualbox postinstall script in examples check whether update guest additions is needed

--- examples/SL6_injection_method/postinstall/virtualbox.sh
+++ examples/SL6_injection_method/postinstall/virtualbox.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -e -E -u -o pipefail; shopt -s failglob;
 
+if [[ -s "adm_envrc" ]]; then
+    . "./adm_envrc"
+else
+    VBOX_VERSION="0.0.0"
+fi
+
+# Do we have any virtualbox guest additions installed ?
+if VBoxControl --version >/dev/null 2>&1; then
+    version=$(VBoxControl --version | cut -f 1 -d"r") # 4.2.12r84980 -> 4.2.12
+    # Expected version is installed, exit
+    if [[ $version == "${VBOX_VERSION}" ]]; then
+        exit 0
+    fi
+fi
+
 # Installing the virtualbox guest additions
 # VBoxGuestAdditions iso is attached (by vbkick) to SATA Controller port 1 device 0
 if [[ -b /dev/sr1 ]]; then
