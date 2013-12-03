125a010a67d22437cdfd8f4ce442954c95863991
wilas <limak.kamwil@gmail.com>
2013-12-04 00:47:51 +0100

    added cfengine to injection postinstall example

--- examples/SL6_injection_method/postinstall/sudo.sh
+++ examples/SL6_injection_method/postinstall/sudo.sh
@@ -2,4 +2,4 @@
 set -e -E -u -o pipefail; shopt -s failglob;
 
 # Allow sudo commands without a tty
-sed -i -re "s/(^Defaults[ ]+requiretty)/#\1/" /etc/sudoers
+sed -i "s/\(^Defaults[ ]\+requiretty\)/#\1/" /etc/sudoers
